95f4d0ac426580578c689437422abdb0f4939aae
Raphael Silva <raphael.silva@sage.com>
2019-07-12 09:42:08 -0300

    Incluindo validacao de dados

--- lib/view/navegacao01View.dart
+++ lib/view/navegacao01View.dart
@@ -10,6 +10,7 @@ class Navegacao01View extends StatefulWidget {
 class _Navegacao01ViewState extends State<Navegacao01View> {
   final _controladorNome = TextEditingController();
   final _controladorIdade = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -19,50 +20,78 @@ class _Navegacao01ViewState extends State<Navegacao01View> {
         title: Text("Navegação 01"),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _controladorNome,
-              decoration: InputDecoration(labelText: "Nome"),
-            ),
-            TextField(
-              controller: _controladorIdade,
-              decoration: InputDecoration(labelText: "Idade"),
-            ),
-            RaisedButton(
-              child: Text("Navegar para view 02 passando parâmetro"),
-              onPressed: () {
-                Route rota = MaterialPageRoute(
-                    builder: (context) => Navegacao02View(
-                          texto: "Raphael",
-                          idade: 30,
-                        ));
+      body: Form(
+        key: _formKey,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFormField(
+                controller: _controladorNome,
+                decoration: InputDecoration(labelText: "Nome", hintText: "Nome"),
+              ),
+              TextFormField(
+                controller: _controladorIdade,
+                decoration: InputDecoration(labelText: "Idade"),
+                validator: (texto) {
+                  int idadeDigitada = int.parse(_controladorIdade.text);
 
-                Navigator.of(context).push(rota);
-              },
-            ),
-            SizedBox(height: 30),
-            RaisedButton(
-              child: Text("Navegar para view 02 SEM parâmetro (ROUTE)"),
-              onPressed: () {
-                Navigator.of(context).pushNamed(NavegacaoHelper.rotaNavegacao02, arguments: null);
-              },
-            ),
-            SizedBox(height: 30),
-            RaisedButton(
-              child: Text("Navegar para view 02 COM parâmetro (ROUTE)"),
-              onPressed: () {
-                Navigator.of(context).pushNamed(
-                  NavegacaoHelper.rotaNavegacao02,
-                  arguments: {"nome": _controladorNome.text, "idade": int.parse(_controladorIdade.text)},
-                );
-              },
-            ),
-            SizedBox(height: 30),
-            RaisedButton(child: Text("Voltar via comando"), onPressed: () => Navigator.pop(context), color: Colors.blue),
-          ],
+                  if (idadeDigitada < 20) {
+                    return "Digite uma idade maior do que 20";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(labelText: "Email"),
+              ),
+              TextFormField(
+                obscureText: true,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: "Senha"),
+              ),
+              RaisedButton(
+                child: Text("Validar dados"),
+                onPressed: () {
+                  bool passouValidacao = _formKey.currentState.validate();
+                  print(passouValidacao);
+                },
+              ),
+              RaisedButton(
+                child: Text("Navegar para view 02 passando parâmetro"),
+                onPressed: () {
+                  Route rota = MaterialPageRoute(
+                      builder: (context) => Navegacao02View(
+                            texto: "Raphael",
+                            idade: 30,
+                          ));
+
+                  Navigator.of(context).push(rota);
+                },
+              ),
+              SizedBox(height: 30),
+              RaisedButton(
+                child: Text("Navegar para view 02 SEM parâmetro (ROUTE)"),
+                onPressed: () {
+                  Navigator.of(context).pushNamed(NavegacaoHelper.rotaNavegacao02, arguments: null);
+                },
+              ),
+              SizedBox(height: 30),
+              RaisedButton(
+                child: Text("Navegar para view 02 COM parâmetro (ROUTE)"),
+                onPressed: () {
+                  Navigator.of(context).pushNamed(
+                    NavegacaoHelper.rotaNavegacao02,
+                    arguments: {"nome": _controladorNome.text, "idade": int.parse(_controladorIdade.text)},
+                  );
+                },
+              ),
+              SizedBox(height: 30),
+              RaisedButton(child: Text("Voltar via comando"), onPressed: () => Navigator.pop(context), color: Colors.blue),
+            ],
+          ),
         ),
       ),
     );
